efb962a8e29ea62a9ec271175be19d2f8d4edcea
KKOA <ka33145@googlemail.com>
2018-04-25 22:47:49 +0100

    Whitelist address fields and added full_address method to Restaurant Model

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     //
-    protected $fillable =['name','description'];
+    protected $fillable =[
+        'name',
+        'description',
+        'address1',
+        'address2',
+        'city',
+        'county',
+        'postcode'];
+
+    public function not_blank($element)
+    {
+        $element = trim($element);
+        return (strlen($element) > 0);
+    }
+    public function full_address()
+    {
+        $parts = [
+            $this->address1,
+            $this->address2,
+            $this->city,
+            $this->county,
+            $this->postcode
+        ];
+        $address = array_filter($parts, [$this,'not_blank']);
+        return implode(', ',$address);
+    }
 }
+
