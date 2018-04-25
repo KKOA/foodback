5436882a940b409ccc5c6ac75d73227851f7ab9d
KKOA <ka33145@googlemail.com>
2018-04-25 02:51:12 +0100

    Added session and errors to master layout. Added references to stylesheets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,7 +26,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        {{--<link rel="stylesheet" href="{{asset('css/master.css')}}">--}}
+        <link rel="stylesheet" href="{{asset('css/master.css')}}">
 
         <!-- Script -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -38,7 +38,7 @@
     <body>
         {{--@include('inc.header')--}}
         <div class='container-fluid'>
-        {{--@include('inc.messages')--}}
+        @include('inc.messages')
             
             @yield('content')
         </div>
