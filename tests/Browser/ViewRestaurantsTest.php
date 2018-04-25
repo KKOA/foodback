6a9a02e309f8a509983748a939ad9c8741961c3f
KKOA <ka33145@googlemail.com>
2018-04-25 22:42:12 +0100

    Added assertion for restaurant address in View Restaurant test

--- tests/Browser/ViewRestaurantsTest.php
+++ tests/Browser/ViewRestaurantsTest.php
@@ -33,19 +33,37 @@ class ViewRestaurantsTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             
             //Create Restaurants
-            $restaurant1 = Restaurant::create(['name' => 'Benny','description' => 'Benny Text']);
-            $restaurant2 = Restaurant::create(['name' => 'Jimmy','description' => 'Jimmy Text']);
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          => 'Benny',
+                    'description'   => 'Benny Text',
+                    'address1'      =>  '47 North Baliey',
+                    'city'          =>  'Durham',
+                    'postcode'      =>  'DH1 3ET'
+                ]
+            );
+            $restaurant2 = Restaurant::create(
+                [
+                    'name'          => 'Jimmy',
+                    'description'   => 'Jimmy Text',
+                    'address1'      =>  '28 Church Road',
+                    'address2'      =>  'Chalkton',
+                    'city'          =>  'Hove',
+                    'postcode'      =>  'BN3 2FN'
+                ]
+            );
 
+            //dd($restaurant2->full_address());
+            //$full_address = $restaurant2->full_address();
             $browser->visit('/')
                     ->assertPathIs('/restaurants')
-                    //->assertURLIs('/restaurants')
                     ->assertSeeIn('h1', 'Restaurants')
                     ->assertDontSee('No restaurants avaliable.')
-                    //->assertSee($restaurant1->name)
-                    //->assertSee($restaurant2->name);
-                    ->assertSeeIn('#restaurant'.$restaurant1->id,$restaurant1->name)
-                    ->assertSeeIn('#restaurant'.$restaurant2->id,$restaurant2->name);
-                    //Check database count 
+                    ->assertSeeIn('#restaurant'.$restaurant1->id,e($restaurant1->name))
+                    ->assertSeeIn('#restaurant'.$restaurant1->id,e($restaurant1->full_address()))
+                    ->assertSeeIn('#restaurant'.$restaurant2->id,e($restaurant2->name))
+                    ->assertSeeIn('#restaurant'.$restaurant2->id,e($restaurant2->full_address()));
+ 
         });
     }
 }
