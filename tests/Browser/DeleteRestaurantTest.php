095d13f2e554252396fa0324d8a6467f6056edde
KKOA <ka33145@googlemail.com>
2018-04-25 23:38:28 +0100

    Updated Delete Restaurant test

--- tests/Browser/DeleteRestaurantTest.php
+++ tests/Browser/DeleteRestaurantTest.php
@@ -19,8 +19,33 @@ class DeleteRestaurantTest extends DuskTestCase
     public function testExample()
     {
         $this->browse(function (Browser $browser) {
-            $restaurant1 = Restaurant::create(['name' => 'Benny','description' => 'Benny Text']);
-            $restaurant2 = Restaurant::create(['name' => 'Jimmy','description' => 'Jimmy Text']);
+            //$restaurant1 = Restaurant::create(['name' => 'Benny','description' => 'Benny Text']);
+            //$restaurant2 = Restaurant::create(['name' => 'Jimmy','description' => 'Jimmy Text']);
+
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          =>  'Infamous Diner',
+                    'description'   =>  'Infamous Diner text',
+                    'address1'      =>  '3-5 Basil Chambers Nicholas Croft',
+                    'address2'      =>  '',
+                    'city'          =>  'Manchester',
+                    'county'        =>  '',
+                    'postcode'      =>  'M4 1EY'
+                ]
+            );
+    
+            $restaurant2 = Restaurant::create(
+                [
+                    'name'          =>  'Los Gatos',
+                    'description'   =>  'Los Gatos text',
+                    'address1'      =>  '1-3 Devizes Road',
+                    'address2'      =>  'Old Town',
+                    'city'          =>  'Swindon',
+                    'county'        =>  '',
+                    'postcode'      =>  'SN4 4BJ'
+                ]
+            );
+
             $name = $restaurant1->name;
             $browser->visit('/restaurants/'.$restaurant1->id)
                     //->clickLink('Delete Restaurant')
