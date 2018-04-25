9781e2120eea76b3b01f605a008344fe248877fc
KKOA <ka33145@googlemail.com>
2018-04-26 00:45:35 +0100

    Added assertion for restaurant address in create Restaurant test

--- tests/Browser/CreateRestaurantTest.php
+++ tests/Browser/CreateRestaurantTest.php
@@ -20,15 +20,25 @@ class CreateRestaurantTest extends DuskTestCase
     public function test_user_cannot_create_restaurant_with_invalid_details()
     {
         $this->browse(function (Browser $browser) {
-            $name = '';
-            $desc = 'a';
+            $name='';
+            $desc='a';
+            $address1='';
+            $city='au';
+            $postcode='sn5 5ef fe6';
             //visit create page
             $browser->visit('/restaurants/create')
                     ->type('name', $name)
                     ->type('description',$desc)
+                    ->type('address1',$address1)
+                    ->type('city',$city)
+                    ->type('postcode',$postcode)
                     ->click('button[type="submit"]')
                     ->assertSee('The name field is required.')
-                    ->assertSee('The description must be at least 3 characters.');
+                    ->assertSee('The description must be at least 3 characters.')
+                    ->assertSee('The address1 field is required.')
+                    ->assertSee('The city must be at least 3 characters.')
+                    ->assertSee('The postcode may not be greater than 10 characters.')
+                    ;
         });
     }
 
@@ -37,10 +47,18 @@ class CreateRestaurantTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $name = 'bebo';
             $desc = 'some random text';
+            $address1='28 Church Road';
+            $city='Hove';
+            $county='East Sussex';
+            $postcode='BN3 2FN';
             //visit create page
             $browser->visit('/restaurants/create')
                     ->type('name', $name)
                     ->type('description',$desc)
+                    ->type('address1',$address1)
+                    ->type('city',$city)
+                    ->type('county',$county)
+                    ->type('postcode',$postcode)
                     ->click('button[type="submit"]')
                     ->assertDontSee('The name field is required.')
                     ->assertDontSee('The description must be at least 3 characters.')
@@ -51,11 +69,32 @@ class CreateRestaurantTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             //Create Restaurant
-            $restaurant1 = Restaurant::create(['name' => 'Benny','description' => 'Benny Text']);
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          =>  'Bear & Billet',
+                    'description'   =>  'some description',
+                    'address1'      =>  '94 Lower Bridge Street',
+                    'address2'      =>  '',
+                    'city'          =>  'Chester',
+                    'county'        =>  '',
+                    'postcode'      =>  'CH1 1RU'
+                ]
+            );
+
+            $name = $restaurant1->name;
+            $desc = 'some random text';
+            $address1='28 Church Road';
+            $address2='Rackton';
+            $city='Hove';
+            $postcode='BN3 2FN';
             //visit create page
             $browser->visit('/restaurants/create')
-                    ->type('name', $restaurant1->name)
-                    ->type('description',$restaurant1->description)
+                    ->type('name', $name)
+                    ->type('description',$desc)
+                    ->type('address1',$address1)
+                    ->type('address2',$address2)
+                    ->type('city',$city)
+                    ->type('postcode',$postcode)
                     ->click('button[type="submit"]')
                     ->assertSee('The name has already been taken.');
         });
