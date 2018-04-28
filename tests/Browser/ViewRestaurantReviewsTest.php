acdfa3b13e7cf7d0ca8a7f7a19e359a7992b3cfc
KKOA <ka33145@googlemail.com>
2018-04-29 00:10:36 +0100

    Add test for when no review exist for a restaurant

--- tests/Browser/ViewRestaurantReviewsTest.php
+++ tests/Browser/ViewRestaurantReviewsTest.php
@@ -20,6 +20,51 @@ class ViewRestaurantReviewsTest extends DuskTestCase
      *
      * @return void
      */
+    use DatabaseMigrations;
+
+    public function test_guest_cannot_see_restaurant_reviews()
+    {
+        $this->browse(function (Browser $browser) {    
+            $faker = \Faker\Factory::create();
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          =>  'Nur',
+                    'description'   =>  'Nur text',
+                    'address1'      =>  '22 Bridge Street',
+                    'address2'      =>  '',
+                    'city'          =>  'Glasgow',
+                    'county'        =>  '',
+                    'postcode'      =>  'G5 9HR'
+                ]
+            );
+
+            $restaurant2 = Restaurant::create(
+                [
+                    'name'          =>  'Thai Garden',
+                    'description'   =>  'Thai Garden text',
+                    'address1'      =>  '100 West Street',
+                    'address2'      =>  '',
+                    'city'          =>  'Bristol',
+                    'county'        =>  '',
+                    'postcode'      =>  'BS3 3LR'
+                ]
+            );
+
+            $review1 = Review::create(
+                [
+                    'restaurant_id' => $restaurant2->id,
+                    'comment'       =>  $faker->paragraph,
+                    'rating'        =>  3
+                ]
+            );
+            $browser->visit('/restaurants/'.$restaurant1->id)
+                ->assertSee('No reviews avaliable for this restaurant')
+                ->assertDontSee($review1->rating)
+                ->assertDontSee($review1->comment)
+                ->assertDontSee($review1->updated_at);
+        });
+    }
+
     public function test_guest_can_see_restaurant_reviews()
     {
         $this->browse(function (Browser $browser) {
@@ -70,7 +115,9 @@ class ViewRestaurantReviewsTest extends DuskTestCase
             ->assertSee($review1->rating)
             ->assertSee($review1->updated_at)
             ->assertDontSee($review2->comment)
-            ->assertDontSee($review2->updated_at);
+            ->assertDontSee($review2->updated_at)
+            ->assertDontSee('No reviews avaliable for this restaurant')
+            ;
             //->assertDontSee($review2->comment);
         });
     }
