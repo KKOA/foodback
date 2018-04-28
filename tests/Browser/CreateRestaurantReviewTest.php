93375728f39885c2de588023694bf8b527a55257
KKOA <ka33145@googlemail.com>
2018-04-29 00:20:03 +0100

    Added tests for creating restaurant reviews

---
+++ tests/Browser/CreateRestaurantReviewTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Browser;
+
+//Models
+use App\Restaurant as Restaurant;
+use App\Review as Review;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateRestaurantReviewTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    use DatabaseMigrations;
+    
+    public function test_user_cannot_create_restaurant_review_with_invalid_details()
+    {
+        $this->browse(function (Browser $browser) {
+            //Create Restaurants
+            $faker = \Faker\Factory::create();
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          =>  'Nur',
+                    'description'   =>  'Nur text',
+                    'address1'      =>  '22 Bridge Street',
+                    'address2'      =>  '',
+                    'city'          =>  'Glasgow',
+                    'county'        =>  '',
+                    'postcode'      =>  'G5 9HR'
+                ]
+            );
+
+            $comment = 'a';
+            //$rating = 3;
+            $browser->visit('/restaurants/'.$restaurant1->id)
+                    ->type('comment',$comment)
+                    //->type('rating', $rating)
+                    ->click('.add-review')
+                    //->click('button[type="submit"]')
+                    ->assertSee('The comment must be at least 3 characters.')
+                    ->assertSee('The rating field is required');
+                    
+        });
+    }
+
+
+    public function test_user_can_create_restaurant_review_with_valid_details()
+    {
+        $this->browse(function (Browser $browser) {
+            //Create Restaurants
+            $faker = \Faker\Factory::create();
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          =>  'Nur',
+                    'description'   =>  'Nur text',
+                    'address1'      =>  '22 Bridge Street',
+                    'address2'      =>  '',
+                    'city'          =>  'Glasgow',
+                    'county'        =>  '',
+                    'postcode'      =>  'G5 9HR'
+                ]
+            );
+
+            $comment = 'My text';
+            $rating = 3;
+            $browser->visit('/restaurants/'.$restaurant1->id)
+                    ->type('comment',$comment)
+                    ->type('rating', $rating)
+                    ->click('.add-review')
+                    //->click('button[type="submit"]')
+                    ->assertSee($comment)
+                    ->assertSee($rating);
+        });
+    }
+}
