eee1be2c7a7f1bec66bac7dbb248153695bf6076
KKOA <ka33145@googlemail.com>
2018-05-02 19:52:48 +0100

    Added default value to rating input field

--- resources/views/inc/reviewform.blade.php
+++ resources/views/inc/reviewform.blade.php
@@ -15,7 +15,7 @@
         {!! Form::label('rating', "Rating : ") !!} <span class="required">*</span>
     </div>
     <div class='col-sm-9'>
-        <input id='rating' class='form-control' name='rating' type="number" step="0.5" min='0.0' max='5.0'/>
+        <input id='rating' class='form-control' name='rating' type="number" step="0.5" min='0.0' max='5.0' value='{{$review->rating}}'/>
         <small>Min: 0, Max: 5, only number</small>
     </div>
 </div>
