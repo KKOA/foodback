a16b54c162f155360616697f170b8d111a8fceed
KKOA <ka33145@googlemail.com>
2018-05-05 00:22:38 +0100

    Updated Database configuration for heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,18 @@
 <?php
 
+$host= getenv('DB_HOST');
+$database = getenv('DB_DATABASE');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+
+if (getenv('APP_ENV') == 'production') {
+    $url = parse_url(env("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -35,18 +48,17 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            //'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'database' => database_path(env('DB_DATABASE', 'database.sqlite')),
             'prefix' => '',
         ],
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
