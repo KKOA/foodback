5991fad658998d3d6ae5e7dc2d096dab5de7f0be
KKOA <ka33145@googlemail.com>
2018-05-06 13:56:04 +0100

    Refactored View Restaurants Test

--- tests/Browser/restaurants/ViewRestaurantsTest.php
+++ tests/Browser/restaurants/ViewRestaurantsTest.php
@@ -17,18 +17,29 @@ class ViewRestaurantsTest extends DuskTestCase
 
     use DatabaseMigrations;
 
-
-    public function test_guest_can_view_no_restaurants()
+    public function test_guest_can_see_no_avaliable_restaurants()
     {
         $this->browse(function (Browser $browser) {
+            $restaurant1 = new Restaurant(
+                [
+                    'name'          => 'Benny',
+                    'description'   => 'Benny Text',
+                    'address1'      =>  '47 North Baliey',
+                    'city'          =>  'Durham',
+                    'postcode'      =>  'DH1 3ET'
+                ]
+            );
             $browser->visit('/')
                     ->assertPathIs('/restaurants')
-                    ->assertSeeIn('h1', 'Restaurants')
-                    ->assertSee('No restaurants avaliable.');
+                    ->assertSee('No restaurants avaliable.')
+                    ->assertMissing('#restaurant1') 
+                    ->assertDontSee($restaurant1->name)
+                    ->assertDontSee($restaurant1->full_address())
+                    ->assertDontSee($restaurant1->description);
         });
     }
 
-    public function test_guest_can_view_restaurants()
+    public function test_guest_can_see_avaliable_restaurants()
     {
         $this->browse(function (Browser $browser) {
             
@@ -53,16 +64,15 @@ class ViewRestaurantsTest extends DuskTestCase
                 ]
             );
 
-            //dd($restaurant2->full_address());
-            //$full_address = $restaurant2->full_address();
             $browser->visit('/')
                     ->assertPathIs('/restaurants')
-                    ->assertSeeIn('h1', 'Restaurants')
-                    ->assertDontSee('No restaurants avaliable.')
                     ->assertSeeIn('#restaurant'.$restaurant1->id,e($restaurant1->name))
                     ->assertSeeIn('#restaurant'.$restaurant1->id,e($restaurant1->full_address()))
                     ->assertSeeIn('#restaurant'.$restaurant2->id,e($restaurant2->name))
-                    ->assertSeeIn('#restaurant'.$restaurant2->id,e($restaurant2->full_address()));
+                    ->assertSeeIn('#restaurant'.$restaurant2->id,e($restaurant2->full_address()))
+                    ->assertSeeIn('.total-restaurants',2)
+                    ->assertDontSee('No restaurants avaliable.')
+                    ;
  
         });
     }
