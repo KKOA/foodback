3abf5846978b462a67e1ccb6e0b86647321c2b7d
KKOA <ka33145@googlemail.com>
2018-05-06 18:55:56 +0100

    Refactored Create Restaurants Test

--- tests/Browser/restaurants/CreateRestaurantTest.php
+++ tests/Browser/restaurants/CreateRestaurantTest.php
@@ -19,50 +19,104 @@ class CreateRestaurantTest extends DuskTestCase
 
     public function test_user_cannot_create_restaurant_with_invalid_details()
     {
+        
         $this->browse(function (Browser $browser) {
-            $name='';
-            $desc='a';
-            $address1='';
-            $city='au';
-            $postcode='sn5 5ef fe6';
+            
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          => 'Benny',
+                    'description'   => 'Benny Text',
+                    'address1'      =>  '47 North Baliey',
+                    'city'          =>  'Durham',
+                    'postcode'      =>  'DH1 3ET'
+                ]
+            );
+
+            $restaurant2 = new Restaurant(
+                [
+                    'name'          => '',
+                    'description'   => 'a',
+                    'address1'      =>  '',
+                    'city'          =>  'au',
+                    'postcode'      =>  'sn5 5ef fe6'
+                ]
+            );
+
             //visit create page
             $browser->visit('/restaurants/create')
-                    ->type('name', $name)
-                    ->type('description',$desc)
-                    ->type('address1',$address1)
-                    ->type('city',$city)
-                    ->type('postcode',$postcode)
+                    ->type('name', $restaurant2->name)
+                    ->type('description',$restaurant2->description)
+                    ->type('address1',$restaurant2->address1)
+                    ->type('city',$restaurant2->city)
+                    ->type('postcode',$restaurant2->postcode)
                     ->click('button[type="submit"]')
                     ->assertSee('The name field is required.')
                     ->assertSee('The description must be at least 3 characters.')
                     ->assertSee('The address1 field is required.')
                     ->assertSee('The city must be at least 3 characters.')
-                    ->assertSee('The postcode may not be greater than 10 characters.')
-                    ;
+                    ->assertSee('The postcode may not be greater than 10 characters.');
+            //Visit homepage
+            $browser->visit('/restaurants')
+                    ->assertSeeIn('.total-restaurants',1)
+                    ->assertPresent('#restaurant1')
+                    ->assertSeeIn('#restaurant1',$restaurant1->name)
+                    ->assertSeeIn('#restaurant1',$restaurant1->full_address())
+                    ->assertMissing('#restaurant2')
+                    ->assertDontSee($restaurant2->name)
+                    ->assertDontSee($restaurant2->full_address())
+            ;
         });
     }
 
     public function test_user_can_create_restaurant_with_valid_details()
     {
         $this->browse(function (Browser $browser) {
-            $name = 'bebo';
-            $desc = 'some random text';
-            $address1='28 Church Road';
-            $city='Hove';
-            $county='East Sussex';
-            $postcode='BN3 2FN';
+
+            $restaurant1 = Restaurant::create(
+                [
+                    'name'          => 'Benny',
+                    'description'   => 'Benny Text',
+                    'address1'      =>  '47 North Baliey',
+                    'city'          =>  'Durham',
+                    'postcode'      =>  'DH1 3ET'
+                ]
+            );
+
+            $restaurant2 = new Restaurant(
+                [
+                    'name'          =>  'bebo',
+                    'description'   =>  'some random text',
+                    'address1'      =>  '28 Church Road',
+                    'city'          =>  'Hove',
+                    'county'        =>  'East Sussex',
+                    'postcode'      =>  'BN3 2FN'
+                ]
+            );
+
             //visit create page
             $browser->visit('/restaurants/create')
-                    ->type('name', $name)
-                    ->type('description',$desc)
-                    ->type('address1',$address1)
-                    ->type('city',$city)
-                    ->type('county',$county)
-                    ->type('postcode',$postcode)
+                    ->type('name', $restaurant2->name)
+                    ->type('description',$restaurant2->description)
+                    ->type('address1',$restaurant2->address1)
+                    ->type('city',$restaurant2->city)
+                    ->type('county',$restaurant2->county)
+                    ->type('postcode',$restaurant2->postcode)
                     ->click('button[type="submit"]')
                     ->assertDontSee('The name field is required.')
                     ->assertDontSee('The description must be at least 3 characters.')
-                    ->assertSee($name ." restaurant created");
+                    ->assertSee($restaurant2->name ." restaurant created")
+                    ->assertSee($restaurant2->description)
+                    ->assertSee($restaurant2->full_address());
+            //Visit homepage
+            $browser->visit('/restaurants')
+                    ->assertSeeIn('.total-restaurants',2)
+                    ->assertPresent('#restaurant1')
+                    ->assertSeeIn('#restaurant1',$restaurant1->name)
+                    ->assertSeeIn('#restaurant1',$restaurant1->full_address())
+                    ->assertPresent('#restaurant2')
+                    ->assertSeeIn('#restaurant2',$restaurant2->name)
+                    ->assertSeeIn('#restaurant2',$restaurant2->full_address())
+                    ;
         });
     }
     public function test_user_cannot_create_restaurant_with_non_unquie_name()
@@ -81,22 +135,36 @@ class CreateRestaurantTest extends DuskTestCase
                 ]
             );
 
-            $name = $restaurant1->name;
-            $desc = 'some random text';
-            $address1='28 Church Road';
-            $address2='Rackton';
-            $city='Hove';
-            $postcode='BN3 2FN';
+            $restaurant2 = new Restaurant(
+                [
+                    'name'          =>  $restaurant1->name,
+                    'description'   =>  'some random text',
+                    'address1'      =>  '28 Church Road',
+                    'address2'      =>  'Rackton',
+                    'city'          =>  'Hove',
+                    'county'        =>  '',
+                    'postcode'      =>  'BN3 2FN'
+                ]
+            );
+
             //visit create page
             $browser->visit('/restaurants/create')
-                    ->type('name', $name)
-                    ->type('description',$desc)
-                    ->type('address1',$address1)
-                    ->type('address2',$address2)
-                    ->type('city',$city)
-                    ->type('postcode',$postcode)
+                    ->type('name', $restaurant2->name)
+                    ->type('description',$restaurant2->desc)
+                    ->type('address1',$restaurant2->address1)
+                    ->type('address2',$restaurant2->address2)
+                    ->type('city',$restaurant2->city)
+                    ->type('postcode',$restaurant2->postcode)
                     ->click('button[type="submit"]')
                     ->assertSee('The name has already been taken.');
+            //Visit homepage
+            $browser->visit('/restaurants')
+            ->assertPresent('#restaurant1')
+            ->assertSeeIn('#restaurant1',$restaurant1->name)
+            ->assertSeeIn('.total-restaurants',1)
+            ->assertMissing('#restaurant2')
+            ->assertDontSee($restaurant2->full_address())
+            ;
         });
     }
 }
