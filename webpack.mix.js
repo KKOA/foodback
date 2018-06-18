10582db7a0143abfa006b1ff0445bbee01daea4e
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-18 18:44:31 +0100

    Added master.js to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/jquery.raty.js', 'public/js')
-
-   .sass('resources/assets/sass/master.scss', 'public/css')
+    .js('resources/assets/js/master.js', 'public/js')
+    .sass('resources/assets/sass/master.scss', 'public/css')
    //.js('resources/assets/js/app.js', 'public/js')
    //.sass('resources/assets/sass/app.scss', 'public/css')
    ;
