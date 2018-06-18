8a102a3bc6c93d900200c54fcba3bcfe8f314796
keith(MacBookPro) <ka33145@googlemail.com>
2018-06-18 18:55:57 +0100

    Added return to top functionality using javascript

---
+++ resources/assets/js/master.js
@@ -0,0 +1,26 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+// require('./bootstrap');
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= 50)
+        {
+            $('#return-to-top').fadeIn(200);
+        }
+        else
+        {
+            $('#return-to-top').fadeOut(200);
+        }
+    });
+    $(document).on('click','#return-to-top',function() {
+        $('body,html').animate({
+            scrollTop : 0
+        },500);
+    });
+});
