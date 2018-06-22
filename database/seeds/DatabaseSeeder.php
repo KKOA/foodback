51536a2224513c09178c58a20e88e91f98e7198d
KKOA <ka33145@googlemail.com>
2018-06-22 16:54:45 +0100

    Added cuisine seeder reference to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Cuisine as Cuisine;
+use App\Restaurant as Restaurant;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CuisineSeeder::class);
         $this->call(RestaurantSeeder::class);
         $this->call(ReviewSeeder::class);
+
+        $faker = Faker\Factory::create();
+
+        $restaurants = Restaurant::all();
+        $cuisines = Cuisine::all()->pluck('id')->toArray();
+        
+        foreach($restaurants as $restaurant)
+        {
+            $restaurant->cuisines()->detach();
+            $randomNumber = $faker->numberBetween($min = 0, $max = 4);
+            $restaurant->cuisines()->attach($faker->randomElements((array) $cuisines, $count = $randomNumber));
+        }
     }
 }
