d0f734199acdc28c9ab1c7b87be6192a983e7bdb
KKOA <ka33145@googlemail.com>
2018-07-16 22:39:14 +0100

    Corrected typo and updated restaurant form structure

--- resources/views/inc/restaurantform.blade.php
+++ resources/views/inc/restaurantform.blade.php
@@ -1,10 +1,10 @@
 <div class='row'>
-    <div class='col-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8 offset-xl-3 col-xl-6'>
+    <div class='col-12 offset-lg-1 col-lg-10 offset-xl-2 col-xl-8'>
         {!! Form::model($restaurant, ['route' => $form['url'],'class'=>'form-horizontal restaurant-form','files'=>'true']) !!}
             <header class='formHeader'>
-                <h2 class='text-center text-white'>
+                <h2 class='text-center text-white pl-1 pr-1'>
                     {{$form['formHeader']}} restaurant
-                </h3>
+                </h2>
             </header>
 
             @if(isset($form['method']))
@@ -13,7 +13,6 @@
 
             {{--  Name --}}
             <div class='form-group row '>
-                {{-- <div class='control-label col-sm-3'> --}}
                 <div class='col-md-3 text-md-right'>
                     {!! Form::label('name', 'Name : ',['class'=>'col-form-label']) !!} 
                     <span class="required">*</span>
@@ -29,7 +28,6 @@
                     {!! Form::label('description', "Description : ",['class'=>'col-form-label']) !!} <span class="required">*</span>
                 </div>
                 <div class='col-md-8'>
-                {{-- {!! Form::textarea('description', $restaurant->description, ['rows' => 10,'class' => 'form-control','placeholder'=>'Restaurant description','id'=>'article-ckeditor']) !!} --}}
                 {!! Form::textarea('description', $restaurant->description, ['rows' => 10,'class' => 'form-control','placeholder'=>'Restaurant description','id'=>'description']) !!}
                 </div>
             </div>
@@ -40,7 +38,7 @@
                     {!! Form::label('address1', "Address1 : ",['class'=>'col-form-label']) !!} <span class="required">*</span>
                 </div>
                 <div class='col-md-8'>
-                {!! Form::text('address1', $restaurant->address1, ['rows' => 10,'class' => 'form-control','placeholder'=>'200 Cheam Road']) !!}
+                {!! Form::text('address1', $restaurant->address1, ['class' => 'form-control','placeholder'=>'200 Cheam Road']) !!}
                 </div>
             </div>
 
@@ -50,7 +48,7 @@
                     {!! Form::label('address2', "Address2 : ",['class'=>'col-form-label']) !!}
                 </div>
                 <div class='col-md-8'>
-                {!! Form::text('address2', $restaurant->address2, ['rows' => 10,'class' => 'form-control','placeholder'=>'Sparcells']) !!}
+                {!! Form::text('address2', $restaurant->address2, ['class' => 'form-control','placeholder'=>'Sparcells']) !!}
                 </div>
             </div>
 
@@ -60,7 +58,7 @@
                     {!! Form::label('city', "City : ",['class'=>'col-form-label']) !!} <span class="required">*</span>
                 </div>
                 <div class='col-md-8'>
-                {!! Form::text('city', $restaurant->city, ['rows' => 10,'class' => 'form-control','placeholder'=>'Bristol']) !!}
+                {!! Form::text('city', $restaurant->city, ['class' => 'form-control','placeholder'=>'Bristol']) !!}
                 </div>
             </div>
 
@@ -70,7 +68,7 @@
                     {!! Form::label('county', "County : ",['class'=>'col-form-label']) !!}
                 </div>
                 <div class='col-md-8'>
-                {!! Form::text('county', $restaurant->county, ['rows' => 10,'class' => 'form-control','placeholder'=>'Wiltshire']) !!}
+                {!! Form::text('county', $restaurant->county, ['class' => 'form-control','placeholder'=>'Wiltshire']) !!}
                 </div>
             </div>
 
@@ -80,16 +78,14 @@
                     {!! Form::label('postcode', "Postcode : ",['class'=>'col-form-label']) !!} <span class="required">*</span>
                 </div>
                 <div class='col-md-8'>
-                {!! Form::text('postcode', $restaurant->postcode, ['rows' => 10,'class' => 'form-control','placeholder'=>'EN1 DG2']) !!}
+                {!! Form::text('postcode', $restaurant->postcode, ['class' => 'form-control','placeholder'=>'EN1 DG2']) !!}
                 </div>
             </div>
-
             
             {{-- Cuisine Type --}}
+            <h3 class='text-center'>Cuisine Type</h3>
             <div class='form-group row justify-content-center'>
-                <h4>Cuisine Type</h4>
-                {{-- <div class='col-md-10 col-md-offset-1'> --}}
-                <div class='center-offset-md-1 col-md-11'>
+                <div class='offset-md-1 col-md-10'>
                     @foreach($cuisines as $cuisine)
                         <label class='cuisine' id='{{$cuisine->name}}' class='text-center'>
                             {{Form::checkbox('cuisines[]', $cuisine->id , $restaurant->cuisines->contains($cuisine->id))}}   
@@ -100,11 +96,13 @@
             </div>
 
             {{-- Preview Image & cover_image text --}}
-
+            <h3 class='text-center'>Photo</h3>
             <div class='form-group row align-items-center'>
                 <div class='control-label col-md-3 text-md-right'>
-                    {!! Form::label('preview_cover_image', "Preview image : ",['class'=>'col-form-label']) !!}
+                    {!! Form::label('preview_cover_image', "Preview image : ",['class'=>'col-form-label']) !!}<br>
+                    <label for='preview_cover_image' class='col-form-label'>Preview<br>Image</label>
                 </div>
+
                 <div class='col-md-8'>
                     @if($restaurant->cover_image)
                         <img src="{{asset('storage/upload/restaurants/'.$restaurant->id.'/'.$restaurant->cover_image)}}" alt="{{$restaurant->name.' cover image'}}"
@@ -123,28 +121,22 @@
                 <div class='col-md-8'>
                     <input type='file' name='cover_image' id='cover_image'>
 
-                    <div><small>Image types only (jpg, png, gif)</small></div>
+                    <div><p><small>Image types only (jpg, png, gif)</small></p></div>
                     <a href="#" id="clear" class='btn btn-secondary'>Reset</a>
                 </div>
             </div>
             <div class='form-group row'>
-                {{-- <div class='col-sm-offset-2 col-sm-9'> --}}
-                {{-- <div class='col-md-offset-1 col-md-10'> --}}
-                {{-- <div class='col-12 offset-md-1 col-md-5 mb-3'> --}}
 
                 @if(isset($form["cancel"]['parameter']))
-                        <a href='{{route($form["cancel"]["url"], $form["cancel"]["parameter"])}}' link_to restaurants_path, class='btn btn-warning btn-lg center-offset-xs-1 col-11 center-offset-md-1 col-md-5 mb-3' title='See all restaurants'>
-                                Cancel <i class='fas fa-times'></i>
-                        </a>
-                    @else
-                        <a href='{{route($form["cancel"]["url"])}}' link_to restaurants_path, class='btn btn-warning btn-lg center-offset-xs-1 col-11 center-offset-md-1 col-md-5 mb-3' title='See all restaurants'>
-                                Cancel <i class='fas fa-times'></i>
-                        </a>
-                    @endif
-                {{-- </div> --}}
-                {{-- <div class='col-12 offset-md-1 col-md-5 mb-3' > --}}
-                    {!! Form::button($form['submission']['text'], ['class' => 'btn btn-success btn-lg center-offset-xs-1 col-11 center-offset-md-1 col-md-5 mb-3','type' => 'submit']) !!}
-                {{-- </div> --}}
+                    <a href='{{route($form["cancel"]["url"], $form["cancel"]["parameter"])}}' link_to restaurants_path, class='btn btn-secondary btn-lg center-offset-xs-1 col center-offset-md-1 mb-3' title='See all restaurants'>
+                            Cancel <i class='fas fa-times'></i>
+                    </a>
+                @else
+                    <a href='{{route($form["cancel"]["url"])}}' link_to restaurants_path, class='btn btn-secondary btn-lg center-offset-xs-1 col center-offset-md-1 mb-3' title='See all restaurants'>
+                            Cancel <i class='fas fa-times'></i>
+                    </a>
+                @endif
+                {!! Form::button($form['submission']['text'], ['class' => 'btn btn-success btn-lg center-offset-xs-1 col center-offset-md-1 mb-3','type' => 'submit']) !!}
             </div>
         {!! Form::close() !!}
     </div>
