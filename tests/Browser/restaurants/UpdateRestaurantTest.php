a8c8762c9ddccf985bd8f373ff74128a984f5a83
KKOA <ka33145@googlemail.com>
2018-07-29 16:19:23 +0100

    Updated validation test for address2 and county in test_owner_cannot_update_their_own_restaurant_with_invalid_details method in Update Restaurant Test

--- tests/Browser/restaurants/UpdateRestaurantTest.php
+++ tests/Browser/restaurants/UpdateRestaurantTest.php
@@ -19,7 +19,7 @@ class UpdateRestaurantTest extends DuskTestCase
     public function test_owner_cannot_update_their_own_restaurant_with_invalid_details()
     {
         $this->browse(function (Browser $browser) {
-
+            $faker = \Faker\Factory::create();
             $restaurant1 = Restaurant::create(
                 [
                     'name'          =>  'Bistro Jacques',
@@ -31,25 +31,31 @@ class UpdateRestaurantTest extends DuskTestCase
             );
 
             $restaurant2 = new Restaurant([
-                'name'          =>  '',
-                'description'   =>  'a',
+                'name'          => '',
+                'description'   => 'a',
                 'address1'      =>  '',
+                'address2'      => $faker->paragraph(30,false),
                 'city'          =>  'au',
+                'county'        => 'sa',
                 'postcode'      =>  'sn5 5ef fe6'
             ]);
 
             //visit create page
             $browser->visit('/restaurants/'.$restaurant1->id.'/edit')
-                    ->type('name', $restaurant2->name)
+                    ->type('name',$restaurant2->name)
                     ->type('description',$restaurant2->description)
                     ->type('address1',$restaurant2->address1)
+                    ->type('address2',$restaurant2->address2)
                     ->type('city',$restaurant2->city)
+                    ->type('county',$restaurant2->county)
                     ->type('postcode',$restaurant2->postcode)
                     ->click('button[type="submit"]')
                     ->assertSee('The name field is required.')
                     ->assertSee('The description must be at least 3 characters.')
                     ->assertSee('The address1 field is required.')
+                    ->assertSee('The address2 may not be greater than 255 characters.')
                     ->assertSee('The city must be at least 3 characters.')
+                    ->assertSee('The county must be empty or atleast 3 characters long.')
                     ->assertSee('The postcode may not be greater than 10 characters.')
                     ->assertDontSee($restaurant2->name ." restaurant updated")
                     ;
