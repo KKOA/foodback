19b95ee872f1827e2cd1dc281f731db0b4f99b6a
KKOA <ka33145@googlemail.com>
2018-12-24 13:18:41 +0000

    Added dummy view and dummy routes for about, support, legal links

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@
 */
 
 Route::get('/', function () {
-    return redirect('/restaurants');
-});
+    // return redirect('/restaurants');
+    return view('welcome');
+})->name('root');
 // Restaurant CRUD routes
 Route::resource('/restaurants','RestaurantController');
 
@@ -23,3 +24,63 @@ Route::resource('restaurants.reviews', 'ReviewController',['except' => ['index',
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/account',function(){
+    $myTitle = 'My Account';
+    return view('temp', compact('myTitle'));
+})->name('account');
+
+//About links
+Route::get('/about',function(){
+    $myTitle = 'About';
+    return view('temp', compact('myTitle'));
+})->name('about');
+
+Route::get('/about/history',function(){
+    $myTitle = 'Company History';
+    return view('temp', compact('myTitle'));
+})->name('about.history');
+
+Route::get('/about/team',function(){
+    $myTitle = 'The Team';
+    return view('temp', compact('myTitle'));
+})->name('about.team');
+
+
+//Support links
+Route::get('/support',function(){
+     $myTitle = 'Support';
+     return view('temp', compact('myTitle'));
+})->name('support');
+
+Route::get('/support/contact',function(){
+     $myTitle = 'Contact';
+     return view('temp', compact('myTitle'));
+})->name('support.contact');
+
+Route::get('/support/faqs',function(){
+     $myTitle = 'FAQs';
+     return view('temp', compact('myTitle'));
+})->name('support.faqs');
+
+
+//Legal links
+Route::get('/legal',function(){
+    $myTitle = 'Legal';
+    return view('temp', compact('myTitle'));
+})->name('legal');
+
+Route::get('/legal/terms',function(){
+     $myTitle = 'Terms & Conditions';
+     return view('temp', compact('myTitle'));
+})->name('legal.terms');
+
+Route::get('/legal/privacy',function(){
+     $myTitle = 'Privacy Policy';
+     return view('temp', compact('myTitle'));
+})->name('legal.privacy');
+
+Route::get('/legal/cookie',function(){
+     $myTitle = 'Cookie Policy';
+     return view('temp', compact('myTitle'));
+})->name('legal.cookie');
