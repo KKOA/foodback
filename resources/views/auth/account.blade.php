f33c7131e3e98d61c97eae8be29b3b7ab678b9e6
KKOA <ka33145@googlemail.com>
2019-01-06 03:49:50 +0000

    Added user account view

---
+++ resources/views/auth/account.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('meta')
+    <meta name="description" content="account">
+    @overwrite
+@section('title', 'Account')
+
+@section('content')
+{{$myTitle}}
+@endsection
