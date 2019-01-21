ef3dc3f8a012ed0aedadd0d4073cb3570705ae8a
KKOA <ka33145@googlemail.com>
2019-01-21 00:20:55 +0000

    Enforced Foreign Key Constraint between restaurants,  restaurant_cuisine and cuisines tables in the database

--- database/migrations/2018_06_21_105038_create_cuisine_restaurant_table.php
+++ database/migrations/2018_06_21_105038_create_cuisine_restaurant_table.php
@@ -15,10 +15,30 @@ class CreateCuisineRestaurantTable extends Migration
     {
         Schema::create('cuisine_restaurant', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('cuisine_id');
-            $table->integer('restaurant_id');
+            $table->integer('cuisine_id')->unsigned();
+            $table->integer('restaurant_id')->unsigned();
             $table->timestamps();
         });
+
+        Schema::table('cuisine_restaurant', function (Blueprint $table) {
+            $table->foreign('cuisine_id')
+            ->references('id')
+            ->on('cuisines')
+            ->onDelete('cascade')
+            ->onUpdate('cascade')
+            ;
+        });
+
+        Schema::table('cuisine_restaurant', function (Blueprint $table) {
+            $table->foreign('restaurant_id')
+            ->references('id')
+            ->on('restaurants')
+            ->onDelete('cascade')
+            ->onUpdate('cascade')
+            ;
+        });
+
+
     }
 
     /**
@@ -28,6 +48,10 @@ class CreateCuisineRestaurantTable extends Migration
      */
     public function down()
     {
+        Schema::table('cuisine_restaurant', function (Blueprint $table) {
+            $table->dropForeign('cuisine_restaurant_cuisine_id_foreign');
+            $table->dropForeign('cuisine_restaurant_restaurant_id_foreign');
+        });
         Schema::dropIfExists('cuisine_restaurant');
     }
 }
