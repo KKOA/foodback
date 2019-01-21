ed7888a03e08c7dca25937781e1c62a78e9fd31a
KKOA <ka33145@googlemail.com>
2019-01-21 00:22:24 +0000

    Added more user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -29,5 +29,33 @@ class UserSeeder extends Seeder
                 'password'      =>  bcrypt('secret')
             ]
         );
+
+        $user3 = User::firstOrCreate(
+            ['name'=>'Sarah'], //Greek
+            [
+                'name'          =>  'Sarah',
+                'email'         => 'sarah@test.com',
+                'password'      =>  bcrypt('secret')
+            ]
+        );
+
+        $user4 = User::firstOrCreate(
+            ['name'=>'Mary'], //Greek
+            [
+                'name'          =>  'Mary',
+                'email'         => 'mary@test.com',
+                'password'      =>  bcrypt('secret')
+            ]
+        );
+
+        $user5 = User::firstOrCreate(
+            ['name'=>'Aaron'], //Greek
+            [
+                'name'          =>  'Aaron',
+                'email'         => 'aaron@test.com',
+                'password'      =>  bcrypt('secret')
+            ]
+        );
+
     }
 }
