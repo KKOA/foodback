53beb744ffc022d6ec409cbe796efb5b3a00cec5
KKOA <ka33145@googlemail.com>
2019-02-04 18:29:05 +0000

    Moved Showing x - y of z on restaurants/index to seperate file

---
+++ resources/views/inc/result_info.blade.php
@@ -0,0 +1,8 @@
+<div class='col-12 text-center font-weight-bold'>
+    Showing 
+    <span class='first-item'>{{$restaurants->firstItem()}}</span>
+    - 
+    <span class='last-item'>{{$restaurants->lastItem()}}</span>
+    of 
+    <span class='total-restaurants'>{{$restaurants->total()}}</span>
+</div>
