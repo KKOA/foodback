ec0510919573da22c8311c50db9ebcd9eaba45fa
keith(Desktop-Work) <ka33145@googlemail.com>
2019-07-30 10:05:18 +0100

    Added addition security to the controller. This prevents one user from modifying or deleting another's review. Also, to stop a restaurant owner from adding a review to their restaurant.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,30 +6,47 @@ namespace App\Http\Controllers;
 //Models
 use App\Models\Restaurant;
 use App\Models\Review;
+use App\Models\Cuisine;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 use \Illuminate\Http\Response;
 use \Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
+use Auth;
 use \Exception;
-use \Illuminate\Contracts\View\Factory;
+
+// use \Illuminate\Contracts\View\Factory;
 
 /**
  * Class ReviewController
  * @package App\Http\Controllers
- */
+ * */
 class ReviewController extends Controller
 {
 
-	/**
+    /** 
+     * Restaurant Constructor
+     * @return void
+     * */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
 	 * Show the form for creating a new resource.
 	 * @param int $id
 	 * @return View
-	 */
-	public function create(int $id) :View
+	 * */
+	public function create(int $id) 
     {
         $restaurant = Restaurant::find($id);
+        if(Auth::user()->id === $restaurant->user->id)
+        {
+            return redirect()->route('restaurants.show',$restaurant->id)->with('error',"Cannot add review to your own restaurant");
+        }
+
         $review = new Review();
         return view('reviews.create', compact('review','restaurant'));
     }
@@ -40,7 +57,7 @@ class ReviewController extends Controller
      * @param  Request     $request
      * @param   int        $id
      * @return RedirectResponse
-     */
+     * */
     public function store(Request $request, int $id) :RedirectResponse
     {
         $validatedData = $request->validate([
@@ -49,6 +66,7 @@ class ReviewController extends Controller
         ]);
 
         $review = new Review();
+        $review->user_id =  Auth::user()->id;
         $review->restaurant_id = $id;
         $review->comment = $request->comment;
         $review->rating = floatval($request->rating);
@@ -63,9 +81,13 @@ class ReviewController extends Controller
 	* @param  int                          $review_id
 	* @return View
 	*/
-    public function edit(int $restaurant_id, int $review_id) :View
+    public function edit(int $restaurant_id, int $review_id)
     {
         $review = Review::find($review_id);
+        if(Auth::user()->id !== $review->user->id)
+        {
+            return redirect()->route('restaurants.show',$restaurant_id);
+        }
         return view('reviews.edit', compact('review'));
     }
 
