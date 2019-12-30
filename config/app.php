282198b2f2de99b1925ca4dc856e68a97f50b511
Keith - Toshiba - GitKraken <ka33145@googlemail.com>
2019-12-30 03:12:22 +0000

    Corrected typo in config/app

--- config/app.php
+++ config/app.php
@@ -22,7 +22,7 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | services the application utilizes. Set this in your ".env" file.
     |
     */
 
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-	'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -95,20 +95,20 @@ return [
 
     'fallback_locale' => 'en',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Faker Locale
-	|--------------------------------------------------------------------------
-	|
-	| This locale will be used by the Faker PHP library when generating fake
-	| data for your database seeds. For example, this will be used to get
-	| localized telephone numbers, street address information and more.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Faker Locale
+    |--------------------------------------------------------------------------
+    |
+    | This locale will be used by the Faker PHP library when generating fake
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
+    |
+    */
 
-		'faker_locale' => 'en_US',
+    'faker_locale' => 'en_US',
 
-	/*
+    /*
     |--------------------------------------------------------------------------
     | Encryption Key
     |--------------------------------------------------------------------------
@@ -168,7 +168,7 @@ return [
         //Intervention Image
         Intervention\Image\ImageServiceProvider::class,
 
-        //htmlmin/htmlmin (html minifer)
+        //htmlmin/htmlmin (HTML Minifer)
         HTMLMin\HTMLMin\HTMLMinServiceProvider::class,
 
         /*
@@ -202,7 +202,7 @@ return [
     'aliases' => [
 
         'App' => Illuminate\Support\Facades\App::class,
-	    'Arr' => Illuminate\Support\Arr::class,
+        'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
@@ -232,7 +232,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
-	    'Str' => Illuminate\Support\Str::class,
+        'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
